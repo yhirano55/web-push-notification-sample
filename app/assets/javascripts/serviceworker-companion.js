03e0ae7e1af140291ce4bb9cfc415c5e377136c8
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-23 13:24:25 +0900

    rails g controller Api::V1::Subscriptions

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,9 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
+    .then(function(registration) {
+      return registration.pushManager.subscribe({userVisibleOnly: true});
+    })
+    .then(function(subscription) {
+      $.post('/api/v1/subscriptions', { subscription: subscription.toJSON() });
     });
 }
