496770945ee544096d152a31e82d29f20c028803
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-23 13:45:28 +0900

    implement Api::V1::SubscriptionsController

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    Subscription.find_or_create_by!(endpoint: permitted_params[:endpoint]) do |subscription|
+      subscription.p256dh = permitted_params.dig(:keys, :p256dh)
+      subscription.auth   = permitted_params.dig(:keys, :auth)
+    end
+
+    head :ok
+  end
+
+  private
+
+  def permitted_params
+    params.require(:subscription).permit(:endpoint, keys: [:p256dh, :auth])
+  end
 end
