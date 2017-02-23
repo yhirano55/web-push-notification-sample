730eaca6965e08c26f99f991624d1e789c67e308
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-23 14:02:43 +0900

    modify serviceworker-companion.js to subscribe notification

--- app/assets/javascripts/serviceworker-companion.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(registration) {
-      return registration.pushManager.subscribe({userVisibleOnly: true});
-    })
-    .then(function(subscription) {
-      $.post('/api/v1/subscriptions', { subscription: subscription.toJSON() });
-    });
-}
